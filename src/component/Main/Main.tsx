a12eb6635aee2e337dab672aa2562e5fc42b318c
vderckdeveloper <vderckdeveloper@gmail.com>
2024-04-03 13:38:06 +0900

    refractor: main

---
+++ src/component/Main/Main.tsx
@@ -0,0 +1,18 @@
+import MainProfie from "./MainProfile";
+import CenterDescription from "../../sharedComponent/Description/CenterDescription";
+
+function Main() {
+
+    const CenterDescriptionContent = {
+
+    }
+
+    return (
+        <>
+            <MainProfie />
+            <CenterDescription title={'a'} content={'b'}/>
+        </>
+    );
+}
+
+export default Main;
