713a732dce949a157e96a93a15f91b503f4f8738
vderckdeveloper <vderckdeveloper@gmail.com>
2024-04-04 19:29:45 +0900

    feat: bottom margin, line boundary, left description, and main capability

--- src/component/Main/Main.tsx
+++ src/component/Main/Main.tsx
@@ -1,16 +1,40 @@
 import MainProfie from "./MainProfile";
+import BottomMargin from "@/sharedComponent/Margin/BottomMargin";
+import LineBoundary from "@/sharedComponent/Boundary/LineBoundary";
+import LeftDescription from "@/sharedComponent/Description/LeftDescription";
+import MainCapability from "./MainCapability";
 import CenterDescription from "../../sharedComponent/Description/CenterDescription";
+
+interface LeftDescriptionContent {
+    title: string;
+    body: string;
+}
+
+interface CenterDescriptionContent {
+    title: string;
+    body: string;
+}
 
 function Main() {
 
-    const CenterDescriptionContent = {
+    const LeftDescriptionContent: LeftDescriptionContent = {
+        title: '전문 능력',
+        body: '갖추고 있는 저의 능력을 소개합니다.'
+    }
 
+    const CenterDescriptionContent: CenterDescriptionContent = {
+        title: '프로젝트',
+        body: '진행했던 프로젝트를 소개합니다.'
     }
 
     return (
         <>
             <MainProfie />
-            <CenterDescription title={'a'} content={'b'}/>
+            <LineBoundary />
+            <BottomMargin />
+            <LeftDescription content={LeftDescriptionContent}/>
+            <MainCapability />
+            <CenterDescription content={CenterDescriptionContent}/>
         </>
     );
 }
