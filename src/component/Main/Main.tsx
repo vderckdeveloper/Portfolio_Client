2ee656b0c5de87452330cff0268b4af0a7e66a7c
vderckdeveloper <vderckdeveloper@gmail.com>
2024-04-05 16:19:36 +0900

    feat: main

--- src/component/Main/Main.tsx
+++ src/component/Main/Main.tsx
@@ -1,43 +1,14 @@
-import MainProfie from "./MainProfile";
-import BottomMargin from "@/sharedComponent/Margin/BottomMargin";
-import LineBoundary from "@/sharedComponent/Boundary/LineBoundary";
-import LeftDescription from "@/sharedComponent/Description/LeftDescription";
-import MainCapability from "./MainCapability";
-import CenterDescription from "../../sharedComponent/Description/CenterDescription";
-import MainProject from "./MainProject";
-
-interface LeftDescriptionContent {
-    title: string;
-    body: string;
-}
-
-interface CenterDescriptionContent {
-    title: string;
-    body: string;
-}
+import Sidebar from "../Sidebar/Sidebar";
+import MainBody from "./MainBody";
+import styles from '@/styles/Main/Main.module.css';
 
 function Main() {
 
-    const LeftDescriptionContent: LeftDescriptionContent = {
-        title: '전문 능력',
-        body: '갖추고 있는 저의 능력을 소개합니다.'
-    }
-
-    const CenterDescriptionContent: CenterDescriptionContent = {
-        title: '프로젝트',
-        body: '진행했던 프로젝트를 소개합니다.'
-    }
-
     return (
-        <>
-            <MainProfie />
-            <LineBoundary />
-            <BottomMargin />
-            <LeftDescription content={LeftDescriptionContent}/>
-            <MainCapability />
-            <CenterDescription content={CenterDescriptionContent}/>
-            <MainProject />
-        </>
+        <section className={styles.wrapper}>
+            <Sidebar />
+            <MainBody />
+        </section>
     );
 }
 
