623218e2a60e291c903fdb628c28ddbf6b33ad34
vderckdeveloper <vderckdeveloper@gmail.com>
2024-04-05 21:35:06 +0900

    feat: side bar

--- src/component/Sidebar/Sidebar.tsx
+++ src/component/Sidebar/Sidebar.tsx
@@ -1,18 +1,43 @@
+import Link from 'next/link';
 import styles from '@/styles/Sidebar/Sidebar.module.css';
 
 function Sidebar() {
 
-    return(
+    return (
         <section className={styles.container}>
             <div className={styles.wrapper}>
-                <div>
-                    
+                <div className={styles.imageBox}>
+
                 </div>
-                <nav>
+                <nav className={styles.categoryBox}>
                     <ol>
-                        <li>홈</li>
-                        <li>자료구조</li>
-                        <li>알고리즘</li>
+                        <Link href={'/'}>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#c4cfde" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                                </svg>
+                                <p>홈</p>
+                            </li>
+                        </Link>
+                        <Link href={'/'}>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#c4cfde" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                                </svg>
+                                <p>자료구조</p>
+                            </li>
+                        </Link>
+                        <Link href={'/'}>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#c4cfde" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                                </svg>
+                                <p>알고리즘</p>
+                            </li>
+                        </Link>
                     </ol>
                 </nav>
             </div>
