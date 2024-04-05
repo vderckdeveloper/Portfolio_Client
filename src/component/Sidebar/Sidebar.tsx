6b725a7343688234fc8a1399992b739d964174cc
vderckdeveloper <vderckdeveloper@gmail.com>
2024-04-05 23:57:16 +0900

    fix: side bar

--- src/component/Sidebar/Sidebar.tsx
+++ src/component/Sidebar/Sidebar.tsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
+import sidebarProfileImage from '../../../public/image/sidebar/240405_sidebarProfile_Image_Ver1.0.png';
 import styles from '@/styles/Sidebar/Sidebar.module.css';
 
 function Sidebar() {
+
+    const MenuItems = {
+        HOME: 'HOME',
+        DATA_STRUCTURE: 'DATA_STRUCTURE',
+    };
+
+    const [menu,setMenu] = useState(MenuItems.HOME);
+
 
     return (
         <section className={styles.container}>
             <div className={styles.wrapper}>
                 <div className={styles.imageBox}>
-
+                    <div>
+                        <Image src={sidebarProfileImage} width={512} height={512} alt='사이드바 프로필 이미지'/>
+                    </div>
                 </div>
                 <nav className={styles.categoryBox}>
                     <ol>
-                        <Link href={'/'}>
+                        <Link className={ menu === MenuItems.HOME ? styles.active : ''} href={'/'} onClick={() => setMenu(MenuItems.HOME)}>
                             <li>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#c4cfde" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
@@ -20,7 +33,7 @@ function Sidebar() {
                                 <p>홈</p>
                             </li>
                         </Link>
-                        <Link href={'/'}>
+                        <Link className={ menu === MenuItems.DATA_STRUCTURE ? styles.active : ''} href={'/'} onClick={() => setMenu(MenuItems.DATA_STRUCTURE)}>
                             <li>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#c4cfde" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
