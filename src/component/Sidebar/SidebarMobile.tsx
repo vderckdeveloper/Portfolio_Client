bed14bc02906fc072078e61dc032f5aa8daee142
vderckdeveloper <vderckdeveloper@gmail.com>
2024-04-11 00:00:05 +0900

    feat: side bar close

--- src/component/Sidebar/SidebarMobile.tsx
+++ src/component/Sidebar/SidebarMobile.tsx
@@ -41,6 +41,12 @@ const SidebarMobile = forwardRef((props: SidebarMobile, ref: SidebarMobileRef) =
     return (
         <section className={styles.container} ref={ref}>
             <div className={styles.wrapper}>
+                {/* 사이드바 닫기 */}
+                <div className={styles.closeBox}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 1024 1024" onClick={onSidebarMenuClose}>
+                        <path fill="#ff014f" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z" />
+                    </svg>
+                </div>
                 {/* 이미지 */}
                 <div className={styles.imageBox}>
                     <div>
