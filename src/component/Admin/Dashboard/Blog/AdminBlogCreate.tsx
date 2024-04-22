28e6bde2d7cc74b3b8c1657806606605e95cdd0f
vderckdeveloper <vderckdeveloper@gmail.com>
2024-04-22 23:15:55 +0900

    feat: draft-js mid save

--- src/component/Admin/Dashboard/Blog/AdminBlogCreate.tsx
+++ src/component/Admin/Dashboard/Blog/AdminBlogCreate.tsx
@@ -37,6 +37,33 @@ function AdminBlogCreate() {
         setEditorState(RichUtils.toggleBlockType(editorState, blockType));
     }
 
+    // prompt for link
+    const promptForLink = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        const currentSelection = editorState.getSelection();
+        if (!currentSelection.isCollapsed()) {
+            const url = window.prompt('Enter the URL');
+            if (url) {
+                const contentState = editorState.getCurrentContent();
+                const contentStateWithEntity = contentState.createEntity(
+                    'LINK',
+                    'MUTABLE',
+                    { url }
+                );
+                const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+                const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
+                const withLink = RichUtils.toggleLink(
+                    newEditorState,
+                    newEditorState.getSelection(),
+                    entityKey
+                );
+                setEditorState(withLink);
+            }
+        } else {
+            alert('Please select text where you want to apply the link.');
+        }
+    };
+
     // focus editor
     useEffect(() => {
         if (editor.current) {
@@ -56,11 +83,15 @@ function AdminBlogCreate() {
                 <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('header-two') }}>H2</button>
                 <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('header-three') }}>H3</button>
                 <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('header-four') }}>H4</button>
-                <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('header-four') }}>H5</button>
-                <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('header-four') }}>H6</button>
+                <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('header-five') }}>H5</button>
+                <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('header-six') }}>H6</button>
+                <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('unstyled') }}>Paragraph</button>
                 <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('unordered-list-item') }}>unordered-list-item</button>
                 <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('ordered-list-item') }}>ordered-list-item</button>
+                <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('blockquote') }}>Blockquote</button>
                 <button onMouseDown={(e) => { e.preventDefault(); onToggleBlockType('code-block') }}>code block</button>
+                <button onMouseDown={promptForLink}>Link</button>
+
             </div>
             <Editor
                 ref={editor}
