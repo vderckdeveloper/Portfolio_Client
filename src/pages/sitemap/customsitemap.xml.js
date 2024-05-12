8cf102fd6a4b207d25cb7ae541a3061b8d126781
vderckdeveloper <vderckdeveloper@gmail.com>
2024-05-12 22:55:37 +0900

    refractor: remove unnecessary code

--- src/pages/sitemap/customsitemap.xml.js
+++ src/pages/sitemap/customsitemap.xml.js
@@ -1,10 +1,10 @@
 import { getServerSideSitemapLegacy } from 'next-sitemap';
 
-export const getServerSideProps = async (context: any) => {
+export const getServerSideProps = async (context) => {
 
     // custom blog site map -- needs to be updated when adding blog post later
-    const blogIndexList: number[] = [5,6,7,10,11,12,23,24,25,28,29,34,35,57,58,59,60];
-    const sitemapFields: any = blogIndexList.map((blogIndex) => {
+    const blogIndexList = [5,6,7,10,11,12,23,24,25,28,29,34,35,57,58,59,60];
+    const sitemapFields = blogIndexList.map((blogIndex) => {
         return {
             loc: `${process.env.NEXT_SITE_MAP_URL}/blog?index=${blogIndex}`,
             lastmod: new Date().toISOString(),
