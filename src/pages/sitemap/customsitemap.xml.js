44c99543964fcf087ea2c73d62f89241f176e5bd
vderckdeveloper <vderckdeveloper@gmail.com>
2024-05-13 13:40:16 +0900

    refractor: next-site-map

--- src/pages/sitemap/customsitemap.xml.js
+++ src/pages/sitemap/customsitemap.xml.js
@@ -3,7 +3,7 @@ import { getServerSideSitemapLegacy } from 'next-sitemap';
 export const getServerSideProps = async (context) => {
 
     // custom blog site map -- needs to be updated when adding blog post later
-    const blogIndexList = [5,6,7,10,11,12,23,24,25,28,29,34,35,57,58,59,60];
+    const blogIndexList = [1,2,3,4,5];
     const sitemapFields = blogIndexList.map((blogIndex) => {
         return {
             loc: `${process.env.NEXT_SITE_MAP_URL}/blog?index=${blogIndex}`,
