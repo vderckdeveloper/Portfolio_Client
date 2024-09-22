f9e9d80e8c862cc110f826b6505c8dd387a6220c
vderckdeveloper <vderckdeveloper@gmail.com>
2024-09-22 17:36:57 +0900

    refractor: slide image name and path

--- src/component/Main/MainPortfolioProject.tsx
+++ src/component/Main/MainPortfolioProject.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import styles from '@/styles/Main/MainPortfolioProject.module.css';
 
 // image
-import mainSeoulUEduProjectSlideImageOne from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(1)_Ver1.0.jpg';
-import mainSeoulUEduProjectSlideImageTwo from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(2)_Ver1.0.jpg';
-import mainSeoulUEduProjectSlideImageThree from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(3)_Ver1.0.jpg';
+import mainPortFolioProjectSlideImageOne from '../../../public/image/main/mainProject/mainPortfolioProject/240922_mainPortFolio_slideImage(1)_Ver1.0.png';
+import mainPortFolioProjectSlideImageTwo from '../../../public/image/main/mainProject/mainPortfolioProject/240922_mainPortFolio_slideImage(2)_Ver1.0.png';
+import mainPortFolioProjectSlideImageThree from '../../../public/image/main/mainProject/mainPortfolioProject/240922_mainPortFolio_slideImage(3)_Ver1.0.png';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -61,17 +61,17 @@ function MainPortfolioProject(props: MainPortfolioProjectProps) {
                         >
                             <SwiperSlide>
                                 <div className={styles.cardview}>
-                                    <Image src={mainSeoulUEduProjectSlideImageOne} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 첫번째 이미지' quality={100} />
+                                    <Image src={mainPortFolioProjectSlideImageOne} width={800} height={600} alt='개인 블로그 및 포트폴리오 슬라이드 첫번째 이미지' quality={100} />
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide>
                                 <div className={styles.cardview}>
-                                    <Image src={mainSeoulUEduProjectSlideImageTwo} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 두번째 이미지' quality={100} />
+                                    <Image src={mainPortFolioProjectSlideImageTwo} width={800} height={600} alt='개인 블로그 및 포트폴리오 슬라이드 두번째 이미지' quality={100} />
                                 </div>
                             </SwiperSlide>
                             <SwiperSlide>
                                 <div className={styles.cardview}>
-                                    <Image src={mainSeoulUEduProjectSlideImageThree} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 세번째 이미지' quality={100} />
+                                    <Image src={mainPortFolioProjectSlideImageThree} width={800} height={600} alt='개인 블로그 및 포트폴리오 슬라이드 세번째 이미지' quality={100} />
                                 </div>
                             </SwiperSlide>
                         </Swiper>
@@ -93,7 +93,7 @@ function MainPortfolioProject(props: MainPortfolioProjectProps) {
                                     개발 기간
                                 </span>
                                 <span>
-                                    3M (3개월)
+                                    1M (1개월)
                                 </span>
                             </div>
                             <div>
@@ -101,7 +101,7 @@ function MainPortfolioProject(props: MainPortfolioProjectProps) {
                                     서비스
                                 </span>
                                 <span>
-                                    웹 / 웹앱
+                                    웹 
                                 </span>
                             </div>
                             <div>
@@ -128,24 +128,21 @@ function MainPortfolioProject(props: MainPortfolioProjectProps) {
                 </div>
                 <div className={styles.line}></div>
                 <div className={styles.bottomSection}>
-                    <p>2024년 5월에서 2023년 6월까지, 개인 블로그 프로젝트를 개발하였습니다.</p>
-                    <p>반응형 플랫폼으로 PC, 태블릿, 모바일에서 전부 이용가능합니다. Next js를 이용하여, 페이지 사이의 이동이 자유로움과 동시에 SEO 최적화가 가능한 비즈니스 플랫폼을 구상하여 설계하였습니다.</p>
-                    <p>2024년 4월 현재, Google Lighthouse 기준으로 SEO 점수는 100점입니다.</p>
+                    <p>2024년 5월에서 2024년 6월까지, 개인 블로그 프로젝트를 개발하였습니다.</p>
+                    <p>반응형 플랫폼으로 PC, 태블릿, 모바일에서 전부 이용가능합니다. Next js를 이용하여, 페이지 사이의 이동이 자유로움과 동시에 SEO 최적화가 가능한 서비스를 구상하여 설계하였습니다.</p>
+                    <p>2024년 6월 현재, Google Lighthouse 기준으로 SEO 점수는 100점입니다.</p>
                     <p>DB 설계의 경우, aqueytool을 이용하여 직접 ERD를 그렸고, 이를 바탕으로 설계를 진행하였습니다.</p>
                     <figure className={styles.imageBox}>
-                        <Image src={mainSeoulUEduProjectSlideImageOne} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 첫번째 이미지' quality={100} />
-                        <figcaption>설계 사진</figcaption>
+                        <Image src={mainPortFolioProjectSlideImageOne} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 첫번째 이미지' quality={100} />
+                        <figcaption>메인 사진</figcaption>
                     </figure>
-                    <p>프로젝트의 설계 구조입니다. 프론트엔드의 폴더 구조 및 백엔드의 폴더 구조를 도식화하여 나타내었고, DB ERD 구조 또한 도식화하여 총 33개의 테이블 관계를 간략히 표현하였습니다.
-                        프론트엔드 프로젝트의 경우, SPA 와 MPA의 장점을 혼합한 Next js를 차용하여 구성하였습니다. 
-                        백엔드 프로젝트의 경우, nest js를 차용하여 구성하였습니다.
-                        DB의 경우 mysql 보다 성능이 뛰어난 maria db를 차용하여 schema를 구성하였습니다.
+                    <p>프로젝트의 메인페이지입니다.
                     </p>
                     <figure className={styles.imageBox}>
-                        <Image src={mainSeoulUEduProjectSlideImageTwo} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 두번째 이미지' quality={100} />
+                        <Image src={mainPortFolioProjectSlideImageTwo} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 두번째 이미지' quality={100} />
                         <figcaption>프로젝트 사진</figcaption>
                     </figure>
-                    <p>프로젝트의 사진입니다. PC, 태블릿, 모바일에서 자유로운 구동이 가능하며, PWA(Progressive Web Application)기술을 차용하여 모바일 앱으로 이용가능합니다.</p>
+                    <p>프로젝트의 사진입니다. PC, 태블릿, 모바일에서 자유로운 구동이 가능합니다.</p>
                 </div>
             </div>
         </section>
