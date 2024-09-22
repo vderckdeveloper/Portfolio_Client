4f1a63f2ab8b81489adf8f383c544d57705280d2
vderckdeveloper <vderckdeveloper@gmail.com>
2024-09-22 17:35:44 +0900

    feat: main seoul u edu slide image four

--- src/component/Main/MainSeoulUEduProject.tsx
+++ src/component/Main/MainSeoulUEduProject.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import styles from '@/styles/Main/MainSeoulUeduProject.module.css';
 
 // image
-import mainSeoulUEduProjectSlideImageOne from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(1)_Ver1.0.jpg';
-import mainSeoulUEduProjectSlideImageTwo from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(2)_Ver1.0.jpg';
-import mainSeoulUEduProjectSlideImageThree from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(3)_Ver1.0.jpg';
+import mainSeoulUEduProjectSlideImageOne from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(1)_Ver1.0.png';
+import mainSeoulUEduProjectSlideImageTwo from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(2)_Ver1.0.png';
+import mainSeoulUEduProjectSlideImageThree from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(3)_Ver1.0.png';
+import mainSeoulUEduProjectSlideImageFour from '../../../public/image/main/mainProject/mainSeoulUEduProject/240417_mainSeoulUEdu_slideImage(4)_Ver1.0.png';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -109,7 +110,7 @@ function MainSeoulUEduProject(props: MainSeoulUEduProjectProps) {
                                     사용 기술
                                 </span>
                                 <span>
-                                    Next JS, Express, Maria DB, Redis, Nginx, Google Cloud Platform
+                                    Next JS, Express, Nest JS, Maria DB, Redis, Nginx, Google Cloud Platform
                                 </span>
                             </div>
                         </section>
@@ -129,20 +130,23 @@ function MainSeoulUEduProject(props: MainSeoulUEduProjectProps) {
                 <div className={styles.line}></div>
                 <div className={styles.bottomSection}>
                     <p>2022년 8월에서 2023년 2월까지 서울 유 에듀의 공식 프로젝트를 개발하였습니다. 기존 워드프레스를 이용하여 개발하였던 사이트를 리뉴얼하여, 성공적으로 플랫폼을 이전하였습니다.</p>
+                    <p>BM 모델은 온라인 강의, 멘토링 시스템, 부트캠프로 세가지 상품에 대해 유저, 크리에이터, 관리자의 상호작용이 가능하도록 개발했습니다.</p>
                     <p>반응형 플랫폼으로 PC, 태블릿, 모바일에서 전부 이용가능합니다. Next js를 이용하여, 페이지 사이의 이동이 자유로움과 동시에 SEO 최적화가 가능한 비즈니스 플랫폼을 구상하여 설계하였습니다.</p>
-                    <p>2024년 4월 현재, Google Lighthouse 기준으로 SEO 점수는 100점입니다.</p>
+                    <p>또한, PWA(Progressive Web Application)기술을 도입하여, 휴대폰 앱으로 다운받아 사용 가능합니다.</p>
+                    <p>2024년 3월 현재, Google Lighthouse 기준으로 SEO 점수는 100점입니다.</p>
                     <p>chat gpt4 api를 삽입하여, 학습자가 실시간으로 질문 / 답변을 할수있도록 만들었고, 서비스에 방문한 모든 유저가 최대 20개의 질문이 가능하도록 하여 접근성을 높였습니다. 유료 유저는 최대 100개까지 질문이 가능합니다.</p>
                     <p>유저가 물건을 담고 결제 이동시까지의 경로를 최적화(로그인후 상품을담고 장바구니에서 결제화면이 열리기까지 5초 이내)하여 편의성을 높였고 토스,이니시스,카카오 결제 모듈을 도입하였습니다.</p>
                     <p>구글 로그인 ,카카오 로그인을 삽입하였습니다.</p>
                     <p>유저 시스템, 크리에이터 시스템, 관리자 시스템등 총 3개의 경로를 지정하여 플랫폼을 개발하였습니다.</p>
                     <p>DB 설계의 경우, aqueytool을 이용하여 직접 ERD를 그렸고, 이를 바탕으로 설계를 진행하였습니다.</p>
                     <figure className={styles.imageBox}>
-                        <Image src={mainSeoulUEduProjectSlideImageOne} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 첫번째 이미지' quality={100} />
+                        <Image src={mainSeoulUEduProjectSlideImageFour} width={800} height={600} alt='서울 유 에듀 프로젝트 슬라이드 첫번째 이미지' quality={100} />
                         <figcaption>설계 사진</figcaption>
                     </figure>
-                    <p>프로젝트의 설계 구조입니다. 프론트엔드의 폴더 구조 및 백엔드의 폴더 구조를 도식화하여 나타내었고, DB ERD 구조 또한 도식화하여 총 33개의 테이블 관계를 간략히 표현하였습니다.
+                    <p>프로젝트의 설계 구조입니다. DB ERD 구조를 도식화하여 총 45개의 테이블 관계를 간략히 표현하였습니다.
                         프론트엔드 프로젝트의 경우, SPA 와 MPA의 장점을 혼합한 Next js를 차용하여 구성하였습니다. 
                         백엔드 프로젝트의 경우, 설계 변경이 자유로운 express를 통해 controller, router, middleware 구조로 구성하였습니다.
+                        (채팅 서버의 경우는 따로 분리하여, Nest JS로 구축하였습니다)
                         DB의 경우 mysql 보다 성능이 뛰어난 maria db를 차용하여 schema를 구성하였습니다.
                     </p>
                     <figure className={styles.imageBox}>
